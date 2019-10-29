4efbb83098c59ffe29faa66920b1442b84ebb366
you10_19 <you10rinjp.youto@gmail.com>
2019-10-29 19:21:18 +0900

    作成しました

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <ToDo />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "./components/HelloWorld.vue";
+import ToDo from "./components/ToDo.vue"
 
 @Component({
   components: {
-    HelloWorld
+    HelloWorld,
+    ToDo
   }
 })
 export default class App extends Vue {}
---
+++ src/components/ToDo.vue
@@ -0,0 +1,32 @@
+<template>
+   <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">Name</th>
+          <th class="text-left">Detail</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item.name">
+          <td>{{ item.name }}</td>
+          <td>{{ item.detail }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script lang = "ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ToDo extends Vue {
+  items: {name: string, detail:string }[] = [
+      {name: "物理学演習", detail: "プリントの中から2題"},
+      {name: "物理学実験", detail: "実験計画書"},
+  ];
+  newItemName: string = "";
+  newItemDetail: string = "";
+}
+</script>
