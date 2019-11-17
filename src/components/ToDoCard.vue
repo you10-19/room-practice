8181b33c750a1f6b925c3cdcdc9cfd81553e8141
you10_19 <you10rinjp.youto@gmail.com>
2019-11-17 21:25:25 +0900

    修正

--- src/components/ToDoCard.vue
+++ src/components/ToDoCard.vue
@@ -1,20 +1,21 @@
 <template>
   <v-card outlined>
     <v-container>
-      <v-row>
+      <v-row no-gutters>
         <v-icon>event</v-icon>
-        <!-- v-iconを使えるようにする -->
-        <v-col align-start>
-          <v-card-title>{{ item.title }}</v-card-title>
-          <v-card-subtitle>{{ item.detail }}</v-card-subtitle>
-          <v-card-text>{{ item.date }}</v-card-text>
-          <v-card-actions>
-            <v-btn v-if="item.done" text @click="item.done = false"
-              >元に戻す</v-btn
-            >
-            <v-btn v-else text @click="complete">完了済みにする</v-btn>
-          </v-card-actions>
-        </v-col>
+        <v-list>
+          <v-subheader>>{{ item.date }}</v-subheader>
+          <v-list-item-content two-line>
+            <v-list-title>{{ item.title }}</v-list-title>
+            <v-list-subtitle>{{ item.detail }}</v-list-subtitle>
+          </v-list-item-content>
+        </v-list>
+        <v-card-actions>
+          <v-btn v-if="item.done" text @click="item.done = false"
+            >元に戻す</v-btn
+          >
+          <v-btn v-else text @click="complete">完了済みにする</v-btn>
+        </v-card-actions>
       </v-row>
     </v-container>
   </v-card>
